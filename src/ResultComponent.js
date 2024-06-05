95de3d2c5db9f9edad0cfd1f8c07c64524d30f8e
ballcap <aalikhan1988@gmail.com>
2024-06-05 17:15:56 +0900

    Update ResultComponent.js

--- src/ResultComponent.js
+++ src/ResultComponent.js
@@ -3,14 +3,12 @@ import { useLocation } from 'react-router-dom';
 
 function ResultComponent() {
   const location = useLocation();
-  const { fname, lname } = location.state || {};
+  const { formData } = location.state || { formData: { f_name: '', l_name: '' } };
 
   return (
-    <div>
-      <h1>Result</h1>
-      <p>First Name: {fname}</p>
-      <p>Last Name: {lname}</p>
-    </div>
+      <div>
+        <h1>Hello {formData.f_name} {formData.l_name}! </h1>
+      </div>
   );
 }
 
